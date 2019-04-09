8950b811382417c351bcb8c51d262b0368504307
Rachel Waltzer <rachelwaltzer@Rachels-MacBook-Pro.local>
2019-04-08 20:02:23 -0400

    open

--- openweather_api.py
+++ openweather_api.py
@@ -3,7 +3,7 @@ import json
 import secrets
 
 try:
-	cache_file1 = open('cache-ticketmaster.json', 'r')
+	cache_file1 = open('cache-openweather.json', 'r')
 	cache_contents1 = cache_file1.read()
 	CACHE_DICTION1 = json.loads(cache_contents1)
 	cache_file1.close()
